4fc39d96a5b9987a00642c8d5d64e863108335a1
Paul <145531241+rubylover4ever@users.noreply.github.com>
2024-05-14 18:11:15 +0300

    Update Tema09mai.cpp

--- Tema09mai.cpp
+++ Tema09mai.cpp
@@ -1,12 +1,4 @@
-
-
 #include <iostream>
-
-inline void WaitEnter()
-{
-    std::cout << "--Apasati ENTER pentru a continua--";
-    while (std::cin.get() != '\n');
-}
 
 
 int main()
@@ -15,21 +7,22 @@ int main()
     int number;
     std::cout << "Introduceti un an aleatoriu: ";
     std::cin >> number;
-    if ( number % 2 == 0)
+    if ( number % 4 == 0)
     {
         std::cout << "Anul " << number << " este bisect.\n";
     }
     else
         std::cout << "Anul " << number << " nu este bisect. \n";
 
-    WaitEnter();
-    std::cin.get() != '\n';
-
+    std::cout << "--Apasati ENTER pentru a continua--";
+    while (std::cin.get() != '\n');
+  
     // tema 2.  
 
     int factorial;
     factorial = 5 * 4 * 3 * 2 * 1;
     std::cout << "5! este egal cu: " << factorial << "\n";
+
     std::cout << "--Apasati ENTER pentru a continua--";
     while (std::cin.get() != '\n');
 
@@ -37,7 +30,8 @@ int main()
     long ForT;
     std::cout << "Avem urmatoare afirmatie: (5! + 5) = (5 * 25) & (5! - 100) = (120 / 10), daca ambele afirmatii sunt adevarate sunt adevarate atunci vom scrie '1' daca nu, '0' \n";
     ForT = (5 * 25) == (factorial += 5) && (factorial-=100) == (200/10 );
+
     std::cout << "--Pentru a afisa raspunsul apasati ENTER--";
-        while (std::cin.get() != '\n');
+    while (std::cin.get() != '\n');
     std::cout << "raspunsunl este: "<< ForT <<"\n";
 
